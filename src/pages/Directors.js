b54e7c67a2f8784a3459c6b1bb0732e3d0de7757
Muhammadwali10 <mohamedmowlid224@yahoo.com>
2024-08-18 21:13:55 +0300

    complete lab

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,16 +1,40 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/directors")
+      .then((response) => response.json())
+      .then((data) => setDirectors(data))
+      .catch((error) => console.error("Error fetching directors:", error));
+  }, []);
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {directors.length > 0 ? (
+          directors.map((director) => (
+            <article key={director.name}>
+              <h2>{director.name}</h2>
+              <ul>
+                {director.movies.map((movie, index) => (
+                  <li key={index}>{movie}</li>
+                ))}
+              </ul>
+            </article>
+          ))
+        ) : (
+          <p>No directors available</p>
+        )}
       </main>
     </>
   );
-};
+}
 
 export default Directors;
